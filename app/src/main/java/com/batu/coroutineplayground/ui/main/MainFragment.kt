bcc89c1fc942511fe46c455a7797fe615545fa33
Batu-Pro16 <david77115@gmail.com>
2021-10-16 23:53:27 +0800

    chore: add related library for coroutine and lifecyle

--- app/src/main/java/com/batu/coroutineplayground/ui/main/MainFragment.kt
+++ app/src/main/java/com/batu/coroutineplayground/ui/main/MainFragment.kt
@@ -1,11 +1,11 @@
 package com.batu.coroutineplayground.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.batu.coroutineplayground.R
 
 class MainFragment : Fragment() {
--- app/src/main/java/com/batu/coroutineplayground/ui/main/MainViewModel.kt
+++ app/src/main/java/com/batu/coroutineplayground/ui/main/MainViewModel.kt
@@ -1,7 +1,16 @@
 package com.batu.coroutineplayground.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+
+    fun loadSomething(){
+        viewModelScope.launch {
+
+        }
+    }
+
 }
